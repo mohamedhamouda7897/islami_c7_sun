c9a325b0e3400fc4a66b68f0b28ca1cacebbbf5e
Mohamed Hamouda <107848993+mohamedhamoudadevop@users.noreply.github.com>
2022-11-16 21:02:59 +0200

    create islami app and write command git init

---
+++ lib/home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routeName = 'Home';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
