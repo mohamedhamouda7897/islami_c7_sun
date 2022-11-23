4cf3054256983637dad5c07da86a4b2741bad238
Mohamed Hamouda <107848993+mohamedhamoudadevop@users.noreply.github.com>
2022-11-23 21:12:50 +0200

    add theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:islami_c7_sun/home.dart';
+import 'package:islami_c7_sun/home/home.dart';
+
+import 'my_theme.dart';
 
 void main() {
   runApp(MyApplication());
@@ -9,8 +11,12 @@ class MyApplication extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: HomeScreen.routeName,
       routes: {HomeScreen.routeName: (context) => HomeScreen()},
+      theme: MyThemeData.lightTheme,
+      darkTheme: MyThemeData.DarkTheme,
+      themeMode: ThemeMode.light,
     );
   }
 }
